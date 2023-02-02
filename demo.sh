9b753c7086fbf2e74addf25295bdb6811a1bf997
Tim te Beek <tim@moderne.io>
2023-02-02 22:37:14 +0100

    Update demo

--- demo.sh
+++ demo.sh
@@ -34,10 +34,10 @@ read -p ""
 
 echo '
 # Next we ensure we are running Java 8 for now.'
-wrap sdk use java 8.0.345-tem
+wrap sdk use java 8.0.352-tem
 
 echo '# Then we upgrade the Maven wrapper for compatibility, and to add some color.'
-wrap ./mvnw wrapper:wrapper -Dmaven=3.8.6
+wrap ./mvnw wrapper:wrapper -Dmaven=3.8.7
 
 
 echo '# And we commit the results to have a clean slate.'
@@ -49,9 +49,9 @@ clear
 echo '# We will migrate the Spring PetClinic to Spring Boot 2.
 # These recipes are in the added Rewrite Spring Module.
 '
-wrap ./mvnw org.openrewrite.maven:rewrite-maven-plugin:4.35.1:run \
+wrap ./mvnw -U org.openrewrite.maven:rewrite-maven-plugin:run \
   -Drewrite.recipeArtifactCoordinates=\
-org.openrewrite.recipe:rewrite-spring:4.28.0 \
+org.openrewrite.recipe:rewrite-spring:LATEST \
   -DactiveRecipes=\
 org.openrewrite.java.spring.boot2.SpringBoot1To2Migration
 
@@ -109,15 +109,14 @@ xml ed -P -L -N x="http://maven.apache.org/POM/4.0.0" -d '//x:build/x:plugins/x:
 clear
 echo '# Next we will upgrade to Java 17 using a different module.
 # The Migrate Java module contains recipes to adopt new language features.'
-wrap ./mvnw org.openrewrite.maven:rewrite-maven-plugin:4.35.1:run \
+wrap ./mvnw -U org.openrewrite.maven:rewrite-maven-plugin:run \
   -Drewrite.recipeArtifactCoordinates=\
-org.openrewrite.recipe:rewrite-migrate-java:1.10.0-SNAPSHOT \
+org.openrewrite.recipe:rewrite-migrate-java:LATEST \
   -DactiveRecipes=\
-org.openrewrite.java.migrate.UpgradeJava17
+org.openrewrite.java.migrate.UpgradeToJava17
 
 echo '# - The compiler source and target versions are updated.
 # - Dependencies have been added for jaxb.
-# - We gained the maven-jdeprscan-plugin plugin to check for deprecations.
 # - Cobertura & Wro4j have been dropped, as they are incompatible.'
 wrap git --no-pager diff pom.xml
 
@@ -126,7 +125,7 @@ echo '# Individually, these might be simple changes.
 # to upgrade our entire application landscape in minutes.
 
 # We update our JDK to now use Java 17'
-wrap sdk use java 17.0.4-tem
+wrap sdk use java 17.0.5-tem
 echo '
 # And with a final commit, we complete the migration.'
 wrap git commit -a -m "Spring Boot 2.7 on Java 17"
